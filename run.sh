fa44c0a385aff813259f720a4ae3e0d87c560ca6
yangyang <mengyangyang@speechocean.com>
2023-03-27 14:46:57 +0800

    Add train script

---
+++ run.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+torchrun --nproc_per_node=8 --master_port=1234 train.py \
+    --model_name_or_path /data/models/LLaMA/7B-hf/ \
+    --data_path ./alpaca_data.json \
+    --bf16 True \
+    --output_dir ./models \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --fsdp "full_shard auto_wrap" \
+    --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
+    --tf32 True
